c9db47ea9bb8aae4506e6da32db7e49516d2585e
OtaviOuu <conta.otaviou@gmail.com>
2025-01-29 11:46:32 -0300

    Add category selection and Scrapy execution in run.py

---
+++ run.py
@@ -0,0 +1,34 @@
+import questionary
+from selectolax.parser import HTMLParser
+import requests
+import subprocess
+
+
+def get_categories():
+    url = "https://www.estantevirtual.com.br/categoria"
+    response = requests.get(
+        url, headers={"User-Agent": "Mozilla/5.0", "Cookie": "foo=bar"}
+    )
+    tree = HTMLParser(response.text)
+    categories = tree.css(".estantes-list-container ul li a")
+    return [c.attrs["href"][1:] for c in categories]
+
+
+def select():
+    categorias = get_categories()
+    selecionadas = questionary.checkbox("Select categories", choices=categorias).ask()
+
+    return selecionadas
+
+
+def run_scrapy(categories):
+    categories_str = ",".join(categories)
+    subprocess.run(
+        ["scrapy", "crawl", "estantevirtual", "-a", f"categories={categories_str}"]
+    )
+
+
+if __name__ == "__main__":
+    selected_categories = select()
+    if selected_categories:
+        run_scrapy(selected_categories)
